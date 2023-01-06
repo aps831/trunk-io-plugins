045da1a2d0956d201813ff2fb61e66630a8e6725
Andrew Spratley <aps831@yahoo.co.uk>
2023-01-06 22:21:22 +0000

    fix: escape output for colours in console

--- actions/commit-branch/commit-branch.sh
+++ actions/commit-branch/commit-branch.sh
@@ -6,7 +6,7 @@ branch="$(git rev-parse --abbrev-ref HEAD)"
 RED='\033[0;31m'
 NOCOL='\033[0m'
 if [[ ${branch} == "${protected_branch}" ]]; then
-  echo "${RED}You are committing directly to the '${protected_branch}' branch${NOCOL}"
+  echo -e "${RED}You are committing directly to the '${protected_branch}' branch${NOCOL}"
   read -r -p "Do you want to proceed? [Y/n] " RESPONSE
   RESPONSE="${RESPONSE:-Y}"
   case "${RESPONSE}" in
