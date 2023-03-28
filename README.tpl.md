f58df93b2dc8984c72a26ade2a1579cbb0719c40
Andrew Spratley <aps831@yahoo.co.uk>
2023-03-28 18:47:21 +0100

    feat: add templated output check

--- README.tpl.md
+++ README.tpl.md
@@ -23,6 +23,7 @@ There are no linters defined.
 | commitizen-prompt-conventional | Prompt for commit message using conventional-changelog style       |
 | commitizen-tools-check         | Ensure commit messages are correctly formatted                     |
 | hardcoding-check               | Check correctness of hardcoded values defined in `hardcoding.json` |
+| templated-output-check         | Check for changes to files created from templates                  |
 
 To add an action, run:
 
---
+++ actions/templated-output-check/templated-output-check.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+is_templated() {
+  file="$1"
+  directory=$(dirname "${file}")
+  filename=$(basename -- "${file}")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+  path="${directory}/${filename}.tpl.${extension}"
+  [[ -f ${path} ]]
+}
+
+filter() {
+  local function="$1"
+  local arg
+  while read -r arg; do
+    "${function}" "${arg}" && echo "${arg}"
+  done
+}
+
+diffs=$(git diff --name-only --cached)
+templates=$(echo "${diffs}" | filter is_templated)
+
+if [[ -n ${templates} ]]; then
+  echo "The following file(s) have been modified but are derived from templates:"
+  echo "${templates}"
+  read -r -p "Do you wish to continue? [yN] " RESPONSE
+  RESPONSE="${RESPONSE:-N}"
+  if [[ ${RESPONSE} == "N" || ${RESPONSE} == "n" ]]; then
+    exit 1
+  fi
+fi
