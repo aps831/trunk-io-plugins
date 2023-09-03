791adfce4d181d80d86b4e8b45d4ac1cfe1d3dbe
aps831 <70425650+aps831@users.noreply.github.com>
2023-09-03 12:51:50 +0100

    fix: test for commitizen config

--- actions/commitizen-prompt-conventional/run.sh
+++ actions/commitizen-prompt-conventional/run.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 RESET='\033[0m'
+RED='\033[0;31m]'
+CONFIG_FILE="${PWD}/.czrc"
+
+# Check for .czrc
+if [[ ! -f ${CONFIG_FILE} ]]; then
+  echo -e "${RED}Ignoring commitizen prompt as '$(basename "${CONFIG_FILE}")' not found${RESET}"
+  exit 0
+fi
 
 if [[ ${1} == ".git/COMMIT_EDITMSG" ]]; then
   exec </dev/tty && cz --hook || true
